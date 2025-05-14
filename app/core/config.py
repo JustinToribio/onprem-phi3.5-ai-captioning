e9a6401fb2cfeb6d0eada74ba9100a7de18e5464
Justin Toribio <toribio.justin@gmail.com>
2025-05-13 17:33:02 -0700

    updated config with keywords for tags

--- app/core/config.py
+++ app/core/config.py
@@ -1,2 +1,7 @@
 model_id = "microsoft/Phi-3.5-vision-instruct"
 user_prompt = "Describe what is shown in this image."
+KEYWORDS = {
+    "sports": ["basketball", "football", "snowboarding"],
+    "automobiles": ["car", "truck", "van"],
+    "toys": ["teddy", "yo-yo", "kite"]
+}
